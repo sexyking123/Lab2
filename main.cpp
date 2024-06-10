36891fef18e9e7eb9daba7f5786c339424b63524
minchul <minchul1685@snu.ac.kr>
2024-06-10 03:01:25 +0000

    clear

--- main.cpp
+++ main.cpp
@@ -1,22 +1,33 @@
-#include<iostream>
-#include<vector>
-
+#include <iostream>
+#define MAX 9
 using namespace std;
 
-int N,M;
-vector<int> vec;
+int n,m;
+int arr[MAX] = {0,};
+bool visited[MAX] = {0,};
 
-void func(int cnt) {
-
-
-
-
-
-
-
+void dfs(int num, int cnt)
+{
+    if(cnt == m)
+    {
+        for(int i = 0; i < m; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for(int i = num; i <= n; i++)
+    {
+        if(!visited[i])
+        {
+            visited[i] = true;
+            arr[cnt] = i;
+            dfs(i+1,cnt+1);
+            visited[i] = false;
+        }
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> n >> m;
+    dfs(1,0);
 }
